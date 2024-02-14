ef40a89e50d37ab318a1f20ea9636238e11e04fd
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-02-14 18:25:29 +0500

    privacy

--- resources/views/frontend/privacy.blade.php
+++ resources/views/frontend/privacy.blade.php
@@ -1,7 +1,126 @@
 @extends('frontend.layout.master')
 @section('website-content')
 @include('frontend.components.banner',[
-  'mainText' => 'terms',
+  'mainText' => 'privacy Policy',
 ])
-  
+  <div class="container mb-5 mt-4">
+    <div class="row">
+        <div class="col-12">
+            <h2><strong>Privacy Policy</strong></h2>
+            <p>Our Privacy Policy was last updated on [___DATE___].</p>
+            <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your
+                information when You use the Service and tells You about Your privacy rights and how the law protects
+                You.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur a provident dolor nihil debitis sit
+                exercitationem itaque eaque. Porro rem tenetur numquam fugit ut sed quaerat, error molestias dolorum
+                maxime.</p>
+            <p><strong>Interpretation and Definitions</strong></p>
+            <p><strong>Interpretation</strong></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eaque suscipit accusantium minima ex
+                doloremque autem, amet impedit vitae fugiat.</p>
+            <p><strong>Definitions</strong></p>
+            <p>For the purposes of this Privacy Policy:</p>
+            <ul>
+                <li>
+                    <p><strong>"Account"</strong> means a unique account created for You to access our Service or parts
+                        of our Service.</p>
+                </li>
+                <li>
+                    <p><strong>"Business"</strong>, for the purpose of the CCPA (California Consumer Privacy Act),
+                        refers to the Company as the legal entity that collects Consumers' personal information and
+                        determines the purposes and means of the processing of Consumers' personal information, or on
+                        behalf of which such information is collected and that alone, or jointly with others, determines
+                        the purposes and means of the processing of consumers' personal information, that does business
+                        in the State of California.</p>
+                </li>
+                <li>
+                    <p><strong>"Company"</strong> (referred to as either "the Company", "We", "Us" or "Our" in this
+                        Agreement) refers to [___COMPANY_INFORMATION___]</p>
+                    <p>For the purpose of the GDPR, the Company is the Data Controller.</p>
+                </li>
+                <li>
+                    <p><strong>"Country"</strong> refers to [___COMPANY_COUNTRY___].</p>
+                </li>
+                <li>
+                    <p><strong>"Consumer"</strong>, for the purpose of the CCPA (California Consumer Privacy Act), means
+                        a natural person who is a California resident. A resident, as defined in the law, includes (1)
+                        every individual who is in the USA for other than a temporary or transitory purpose, and (2)
+                        every individual who is domiciled in the USA who is outside the USA for a temporary or
+                        transitory purpose.</p>
+                </li>
+                <li>
+                    <p><strong>"Cookies"</strong> are small files that are placed on Your computer, mobile device or any
+                        other device by a website, containing the details of Your browsing history on that website among
+                        its many uses.</p>
+                </li>
+                <li>
+                    <p><strong>"Data Controller"</strong>, for the purposes of the GDPR (General Data Protection
+                        Regulation), refers to the Company as the legal person which alone or jointly with others
+                        determines the purposes and means of the processing of Personal Data.</p>
+                </li>
+                <li>
+                    <p><strong>"Device"</strong> means any device that can access the Service such as a computer, a
+                        cellphone or a digital tablet.</p>
+                </li>
+                <li>
+                    <p><strong>"Do Not Track"</strong> (DNT) is a concept that has been promoted by US regulatory
+                        authorities, in particular the U.S. Federal Trade Commission (FTC), for the Internet industry to
+                        develop and implement a mechanism for allowing internet users to control the tracking of their
+                        online activities across websites.</p>
+                </li>
+                <li>
+                    <p><strong>"Personal Data"</strong> is any information that relates to an identified or identifiable
+                        individual.</p>
+                    <p>For the purposes of GDPR, Personal Data means any information relating to You such as a name, an
+                        identification number, location data, online identifier or to one or more factors specific to
+                        the physical, physiological, genetic, mental, economic, cultural or social identity.</p>
+                    <p>For the purposes of the CCPA, Personal Data means any information that identifies, relates to,
+                        describes or is capable of being associated with, or could reasonably be linked, directly or
+                        indirectly, with You.</p>
+                </li>
+                <li>
+                    <p><strong>"Sale"</strong>, for the purpose of the CCPA (California Consumer Privacy Act), means
+                        selling, renting, releasing, disclosing, disseminating, making available, transferring, or
+                        otherwise communicating orally, in writing, or by electronic or other means, a Consumer's
+                        personal information to another business or a third party for monetary or other valuable
+                        consideration.</p>
+                </li>
+                <li>
+                    <p><strong>"Service"</strong> refers to the Website.</p>
+                </li>
+                <li>
+                    <p>If You have reason to believe that a child under the age of 13 (or 16) has provided Us with
+                        personal information, please contact Us with sufficient detail to enable Us to delete that
+                        information.</p>
+                    <p><strong>Your Rights under the CCPA</strong></p>
+                    <p>The CCPA provides California residents with specific rights regarding their personal information.
+                        If You are a resident of California, You have the following rights:</p>
+                    <ul>
+                        <li><strong>The right to notice.</strong> You have the right to be notified which categories of
+                            Personal Data are being collected and the purposes for which the Personal Data is being
+                            used.</li>
+                        <li><strong>The right to request.</strong> Under CCPA, You have the right to request that We
+                            disclose information to You about Our collection, use, sale, disclosure for business
+                            purposes and share of personal information. Once We receive and confirm Your request, We
+                            will disclose to You:
+                            <ul>
+                                <li>The categories of personal information We collected about You</li>
+                                <li>The categories of sources for the personal information We collected about You</li>
+                                <li>Our business or commercial purpose for collecting or selling that personal
+                                    information</li>
+                                <li>The categories of third parties with whom We share that personal information</li>
+                                <li>The specific pieces of personal information We collected about You</li>
+                                <li>If we sold Your personal information or disclosed Your personal information for a
+                                    business purpose, We will disclose to You:
+                                    <ul>
+                                        <li>The categories of personal information categories sold</li>
+                                        <li>The categories of personal information categories disclosed</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <p>For any personal or non personal query <b><a href="{{Route('contact')}}">Contact Us</a></b>.</div>
+        </div>
+    </div>
 @endsection
